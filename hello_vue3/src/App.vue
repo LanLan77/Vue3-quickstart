9bbf53b76f42b6bae8969eaa0e36d6ddfe4059de
LanLan77 <775172380@qq.com>
2024-04-17 20:11:38 +0930

    编写App组件

---
+++ hello_vue3/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+     <!-- html结构 --> 
+     <div class="app">
+        <h1>你好啊 这是我第一个Vue3项目</h1>
+     </div>
+</template>
+
+<script lang="ts">
+    // JS或者TS
+
+    //export暴露
+    export default {
+        name:"App" //组件名  
+    }
+</script>
+
+<style>
+    /* 样式 */
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding:20px; 
+    }
+</style>
+    
