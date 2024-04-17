285d3b1ed2cef46d53c1c8d09812a74d1d13d143
LanLan77 <775172380@qq.com>
2024-04-17 21:10:50 +0930

    一个简单的效果

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-     <!-- html结构 --> 
-     <div class="app">
-        <h1>你好啊 这是我第一个Vue3项目</h1>
-     </div>
+    <!-- html结构 --> 
+    <div class="app">
+       <h1>你好啊 这是我第一个Vue3项目</h1>
+       <Person/>
+    </div>
 </template>
 
 <script lang="ts">
-    // JS或者TS
+    import Person from './components/Person.vue'
+   // JS或者TS
 
-    //export暴露
-    export default {
-        name:"App" //组件名  
-    }
+   //export暴露
+   export default {
+       name:"App",//组件名  
+       components:{Person} // 注册组件
+   }
 </script>
 
 <style>
-    /* 样式 */
-    .app {
-        background-color: #ddd;
-        box-shadow: 0 0 10px;
-        border-radius: 10px;
-        padding:20px; 
-    }
+   /* 样式 */
+   .app {
+       background-color: #ddd;
+       box-shadow: 0 0 10px;
+       border-radius: 10px;
+       padding:20px; 
+   }
 </style>
-    
+   
